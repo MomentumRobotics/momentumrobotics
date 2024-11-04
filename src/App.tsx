33f9d72b52295de868cb43003878b3de0b7727fa
DESKTOP-I176V4D\Mihai <0ursachemihaiandrei0@gmail.com>
2024-11-04 22:38:27 +0200

    no message

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import logo from './images/logo.png';
+import instagram from './images/instagram.png';
+import './App.css';
+import {Box, Button, Paper, Stack, Typography} from "@mui/material";
+
+function App() {
+    return (
+        <Box paddingTop={"25px"}>
+            <Stack direction={"row"} spacing={"1rem"} position={"sticky"} paddingTop={"25px"} top={0}>
+                <Button variant={"contained"}>Home</Button>
+                <Button variant={"contained"}>Info</Button>
+            </Stack>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+                <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+        </Box>
+    );
+}
+
+export default App;
