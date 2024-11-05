1462acc7067fb5f4a145403d01529dbacd25b186
DESKTOP-I176V4D\Mihai <0ursachemihaiandrei0@gmail.com>
2024-11-05 12:13:40 +0200

    -Added Logo (Instagram)

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import logo from './images/logo.png';
 import instagram from './images/instagram.png';
 import './App.css';
 import {Box, Button, Paper, Stack, Typography} from "@mui/material";
+import {red} from "@mui/material/colors";
 
 function App() {
     return (
         <Box paddingTop={"25px"}>
             <Stack direction={"row"} spacing={"1rem"} position={"sticky"} paddingTop={"25px"} top={0}>
-                <Button variant={"contained"}>Home</Button>
+                <img src={logo} width={"40px"} height={"40px"} />
+                <Button variant={"outlined"}>Home</Button>
                 <Button variant={"contained"}>Info</Button>
             </Stack>
-            <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
+            <Typography variant={"h5"} textAlign={"center"}>Momentum</Typography>
             <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
             <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
             <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
@@ -70,7 +72,7 @@ function App() {
                 <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
                 <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
                 <Typography variant={"h2"} textAlign={"center"}>Momentum</Typography>
-            <img src={instagram} sizes={"small"}/>
+            <img src={instagram}></img>
         </Box>
     );
 }
